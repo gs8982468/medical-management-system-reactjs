f7c1054d9aadcbb89c63484139476c2e1dc3b705
Subhankar Guchhait <gsubhankar2018@gmail.com>
2022-12-11 19:38:56 +0530

    COnfig Routing

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import './App.css';
 import FetchUserDetails from './component/customerComponent/FetchUserDetails';
 import HeaderComponent from './component/customerComponent/HeaderComponent';
 import FooterComponent from './component/customerComponent/FooterComponent';
+import { BrowserRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    {/* <div className="container"> */}
-       <HeaderComponent />
-      <FetchUserDetails />
-
-
-      <FooterComponent />
+      <Router>
+        <div className="container">
+          <HeaderComponent />
+          <div className="container">
+            <Switch>
+              <FetchUserDetails />
+            </Switch>
+          </div>
+          <FooterComponent />
+        </div>
+      </Router >
     </div>
+
   );
 }
 
